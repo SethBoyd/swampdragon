5a4b591a3e017c2a56a8277c0a4c4a07a195e052
Seth Boyd <sethdboyd@gmail.com>
2016-06-10 12:15:10 -0600

    added tests to display failing one2one scenario

--- tests/test_model_serializer_deserialize_one2one.py
+++ tests/test_model_serializer_deserialize_one2one.py
@@ -18,7 +18,7 @@ class FooSerializer(ModelSerializer):
 
     class Meta:
         model = FooOne2One
-        update_fields = ('name', 'bar')
+        update_fields = ('name', 'bar', 'id')
 
 
 class BarSerializer(ModelSerializer):
@@ -26,7 +26,7 @@ class BarSerializer(ModelSerializer):
 
     class Meta:
         model = BarOne2One
-        update_fields = ('number', 'foo')
+        update_fields = ('number', 'foo', 'id')
 
 
 class TestModelSerializer(DragonTestCase):
@@ -49,3 +49,51 @@ class TestModelSerializer(DragonTestCase):
         bar = serializer.save()
         self.assertEqual(bar.number, data['number'])
         self.assertEqual(bar.foo.name, data['foo']['name'])
+
+    def test_deserialize_update_with_one_2_one(self):
+        foo = FooOne2One(name="foo")
+        foo.save()
+        bar = BarOne2One(number=123, foo=foo)
+        bar.save()
+        data = {
+            'id': foo.id,
+            'name': 'updatefoo',
+            'bar': {
+                'id': bar.id,
+                'number': 321,
+            }
+        }
+        serializer = FooSerializer(data=data, instance=bar)
+        foo_serialized = serializer.save()
+
+        #ensure these changes were saved to correct instance
+        bar_check = BarOne2One.objects.get(pk=bar.id)
+        self.assertEqual(foo_serialized.name, data['name'])
+        self.assertEqual(foo_serialized.bar.id, data['bar']['id'])
+        self.assertEqual(foo_serialized.bar.number, data['bar']['number'])
+        self.assertEqual(bar_check.id, data['bar']['id'])
+        self.assertEqual(bar_check.number, data['bar']['number'])
+
+    def test_deserialize_update_with_reverse_one_2_one(self):
+        foo = FooOne2One(name="foo")
+        foo.save()
+        bar = BarOne2One(number=123, foo=foo)
+        bar.save()
+        data = {
+            'id': bar.id,
+            'number': 321,
+            'foo': {
+                'id': foo.id,
+                'name': 'updatefoo'
+            }
+        }
+        serializer = BarSerializer(data=data, instance=bar)
+        bar_serialized = serializer.save()
+
+        #ensure these changes were saved to correct instance
+        foo_check = FooOne2One.objects.get(pk=foo.id)
+        self.assertEqual(bar_serialized.number, data['number'])
+        self.assertEqual(bar_serialized.foo.id, data['foo']['id'])
+        self.assertEqual(bar_serialized.foo.name, data['foo']['name'])
+        self.assertEqual(foo_check.id, data['foo']['id'])
+        self.assertEqual(foo_check.name, data['foo']['name'])
